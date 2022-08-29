a8cf48e19ab9ae4defbfb05d2b3f4871b01fac71
이유진A <yujin092444@gmail.com>
2022-08-29 10:53:29 +0900

    8/27 ViewController 사이에서 데이터 전달

--- ScreenTransitionExample/CodePushViewController.swift
+++ ScreenTransitionExample/CodePushViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class CodePushViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            nameLabel.text = name
+        }
     }
     @IBAction func tapBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
